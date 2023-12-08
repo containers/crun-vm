7f532eb29f6e56bccc26952ee55679e9143db86e
Alberto Faria <afaria@redhat.com>
2023-12-08 18:27:44 +0000

    build.rs: Make it simpler *and* faster
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- build.rs
+++ build.rs
@@ -2,74 +2,55 @@
 
 use std::env;
 use std::error::Error;
-use std::fs;
 use std::io;
-use std::path::Path;
 use std::process::{Command, ExitStatus, Stdio};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=runner/");
 
-    // check if runner Containerfile *contents* changed (Cargo checks timestamps)
+    // build runner image
 
-    let containerfile_copy = Path::new(&env::var("OUT_DIR")?).join("runner.Containerfile");
-
-    let status = Command::new("cmp")
+    let output = Command::new("podman")
+        .arg("image")
+        .arg("build")
         .arg("--quiet")
-        .arg("runner/Containerfile")
-        .arg(&containerfile_copy)
+        .arg("runner/")
+        .stdout(Stdio::piped())
+        .output()?;
+    check_status(output.status)?;
+
+    let image_id = std::str::from_utf8(&output.stdout)?.trim();
+
+    // create container from runner image
+
+    let output = Command::new("podman")
+        .arg("container")
+        .arg("create")
+        .arg(image_id)
+        .stdout(Stdio::piped())
+        .output()?;
+    check_status(output.status)?;
+
+    let container_id = std::str::from_utf8(&output.stdout)?.trim();
+
+    // extract container's root filesystem
+
+    let result = extract_root(container_id);
+
+    // remove container
+
+    let status = Command::new("podman")
+        .arg("container")
+        .arg("rm")
+        .arg("--ignore")
+        .arg("--force")
+        .arg("--time=0")
+        .arg(container_id)
         .spawn()?
         .wait()?;
+    check_status(status)?;
 
-    if status.code().unwrap() != 0 {
-        // build runner image into an OCI archive
-
-        let status = Command::new("podman")
-            .arg("image")
-            .arg("build")
-            .arg("--tag")
-            .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
-            .arg("--squash-all") // TODO: this resolves a podman build failure but should probably be removed
-            .arg("runner/")
-            .spawn()?
-            .wait()?;
-        check_status(status)?;
-
-        // create a container from that OCI archive
-
-        let output = Command::new("podman")
-            .arg("container")
-            .arg("create")
-            .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
-            .stdout(Stdio::piped())
-            .output()?;
-        check_status(output.status)?;
-
-        let container_id = std::str::from_utf8(&output.stdout)?.trim();
-
-        // extract the container's root filesystem
-
-        let result = extract_root(container_id);
-
-        // remove the container
-
-        let status = Command::new("podman")
-            .arg("container")
-            .arg("rm")
-            .arg("--ignore")
-            .arg("--force")
-            .arg("--time=0")
-            .arg(container_id)
-            .spawn()?
-            .wait()?;
-        check_status(status)?;
-
-        result?;
-
-        fs::copy("runner/Containerfile", &containerfile_copy)?;
-    }
-
-    Ok(())
+    result
 }
 
 fn extract_root(container_id: &str) -> Result<(), Box<dyn Error>> {
