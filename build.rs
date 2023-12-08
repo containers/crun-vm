1c0d3258e6e0bdc3121997c7a3e437a7c0a449f3
Alberto Faria <afaria@redhat.com>
2023-12-08 03:55:06 +0000

    build.rs: Do nothing if runner/Containerfile hasn't changed
    We were already using `cargo:rerun-if-changed=runner/`, but that
    triggers a rebuild whenever timestamps change, even if file contents
    haven't.
    
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- build.rs
+++ build.rs
@@ -2,55 +2,74 @@
 
 use std::env;
 use std::error::Error;
+use std::fs;
 use std::io;
+use std::path::Path;
 use std::process::{Command, ExitStatus, Stdio};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=runner/");
 
-    // build runner image into an OCI archive
+    // check if runner Containerfile *contents* changed (Cargo checks timestamps)
 
-    let status = Command::new("podman")
-        .arg("image")
-        .arg("build")
-        .arg("--tag")
-        .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
-        .arg("--squash-all") // TODO: this resolves a podman build failure but should probably be removed
-        .arg("runner/")
+    let containerfile_copy = Path::new(&env::var("OUT_DIR")?).join("runner.Containerfile");
+
+    let status = Command::new("cmp")
+        .arg("--quiet")
+        .arg("runner/Containerfile")
+        .arg(&containerfile_copy)
         .spawn()?
         .wait()?;
-    check_status(status)?;
 
-    // create a container from that OCI archive
+    if status.code().unwrap() != 0 {
+        // build runner image into an OCI archive
 
-    let output = Command::new("podman")
-        .arg("container")
-        .arg("create")
-        .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
-        .stdout(Stdio::piped())
-        .output()?;
-    check_status(output.status)?;
+        let status = Command::new("podman")
+            .arg("image")
+            .arg("build")
+            .arg("--tag")
+            .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
+            .arg("--squash-all") // TODO: this resolves a podman build failure but should probably be removed
+            .arg("runner/")
+            .spawn()?
+            .wait()?;
+        check_status(status)?;
 
-    let container_id = std::str::from_utf8(&output.stdout)?.trim();
+        // create a container from that OCI archive
 
-    // extract the container's root filesystem
+        let output = Command::new("podman")
+            .arg("container")
+            .arg("create")
+            .arg(&format!("oci-archive:{}/runner.oci", env::var("OUT_DIR")?))
+            .stdout(Stdio::piped())
+            .output()?;
+        check_status(output.status)?;
 
-    let result = extract_root(container_id);
+        let container_id = std::str::from_utf8(&output.stdout)?.trim();
 
-    // remove the container
+        // extract the container's root filesystem
 
-    let status = Command::new("podman")
-        .arg("container")
-        .arg("rm")
-        .arg("--ignore")
-        .arg("--force")
-        .arg("--time=0")
-        .arg(container_id)
-        .spawn()?
-        .wait()?;
-    check_status(status)?;
+        let result = extract_root(container_id);
 
-    result
+        // remove the container
+
+        let status = Command::new("podman")
+            .arg("container")
+            .arg("rm")
+            .arg("--ignore")
+            .arg("--force")
+            .arg("--time=0")
+            .arg(container_id)
+            .spawn()?
+            .wait()?;
+        check_status(status)?;
+
+        result?;
+
+        fs::copy("runner/Containerfile", &containerfile_copy)?;
+    }
+
+    Ok(())
 }
 
 fn extract_root(container_id: &str) -> Result<(), Box<dyn Error>> {
