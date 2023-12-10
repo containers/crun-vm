2eb385b7040050b2e08c636d9ec8e151c268a9d9
Alberto Faria <afaria@redhat.com>
2023-12-10 10:44:37 +0000

    tests: Add simple tests that run podman-run
    Signed-off-by: Alberto Faria <afaria@redhat.com>

---
+++ tests/run.rs
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+mod util;
+
+use util::{expect_failure, expect_success, podman, podman_run, REPO_PATH};
+
+#[test]
+fn podman_run_raw() {
+    let container_name = "crun-qemu-test-podman_run_raw";
+
+    expect_success(podman_run([
+        "--name",
+        container_name,
+        "-dit",
+        "quay.io/kubevirt/alpine-container-disk-demo",
+        "unused",
+    ]));
+
+    expect_success(podman(["rm", "--force", "--time=0", container_name]));
+}
+
+#[test]
+fn podman_run_qcow2() {
+    let container_name = "crun-qemu-test-podman_run_qcow2";
+
+    expect_success(podman_run([
+        "--name",
+        container_name,
+        "-dit",
+        "quay.io/containerdisks/fedora:39",
+        "unused",
+    ]));
+
+    expect_success(podman(["rm", "--force", "--time=0", container_name]));
+}
+
+#[test]
+fn podman_run_invalid() {
+    let container_name = "crun-qemu-test-podman_run_invalid";
+
+    let output = podman_run(["--name", container_name, "-dit", "fedora:39", "unused"]);
+
+    let _ = podman(["rm", "--force", "--time=0", container_name]);
+
+    expect_failure(output);
+}
+
+#[test]
+fn podman_run_mounts() {
+    let container_name = "crun-qemu-test-podman_run_mounts";
+
+    expect_success(podman_run([
+        "--name",
+        container_name,
+        "-dit",
+        &format!("-v={}/examples/cloud-init/config:/cloud-init", REPO_PATH),
+        &format!("-v={}/util:/my-tag", REPO_PATH),
+        "quay.io/containerdisks/fedora:39",
+        "unused",
+    ]));
+
+    expect_success(podman(["rm", "--force", "--time=0", container_name]));
+}
---
+++ tests/util/mod.rs
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+use std::io::{self, Write};
+use std::process::{Command, Output};
+
+pub const BINARY_PATH: &str = env!("CARGO_BIN_EXE_crun-qemu");
+pub const REPO_PATH: &str = env!("CARGO_MANIFEST_DIR");
+
+pub fn expect_success(output: Output) {
+    if !output.status.success() {
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stdout().write_all(&output.stderr).unwrap();
+    }
+
+    assert!(output.status.success());
+}
+
+pub fn expect_failure(output: Output) {
+    if output.status.success() {
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stdout().write_all(&output.stderr).unwrap();
+    }
+
+    assert!(!output.status.success());
+}
+
+#[must_use]
+pub fn podman<'a>(args: impl IntoIterator<Item = &'a str>) -> Output {
+    Command::new("podman")
+        .arg("--log-level=debug")
+        .arg(format!("--runtime={}", BINARY_PATH))
+        .args(args)
+        .env("RUST_BACKTRACE", "1")
+        .output()
+        .unwrap()
+}
+
+#[must_use]
+pub fn podman_run<'a>(args: impl IntoIterator<Item = &'a str>) -> Output {
+    podman(
+        ["run", "--security-opt=label=disable"]
+            .into_iter()
+            .chain(args),
+    )
+}
