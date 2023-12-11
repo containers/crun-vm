ee6680e0b539fa9b8bd24ba6ed021a6001e3833f
Alberto Faria <afaria@redhat.com>
2023-12-11 11:47:58 +0000

    util: Add script to extract the VM image packaged in a container image
    Signed-off-by: Alberto Faria <afaria@redhat.com>

---
+++ util/extract-vm-image.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+
+set -o errexit -o pipefail -o nounset
+
+if (( $# != 2 )); then
+    >&2 echo "Usage: $0 <container_image_tag> <output_vm_image_file>"
+    >&2 echo "Extract a VM image file from a container image."
+    exit 2
+fi
+
+container_image_tag=$1
+output_vm_image_file=$2
+
+temp_dir=$( mktemp -d )
+trap 'rm -fr "${temp_dir}"' EXIT
+
+container_id=$( podman container create --quiet "${container_image_tag}" unused )
+trap 'podman container rm "${container_id}" >/dev/null; rm -fr "${temp_dir}"' EXIT
+
+podman container export -o "${temp_dir}/root.tar" "${container_id}"
+mapfile -t candidates < <( tar -tf "${temp_dir}/root.tar" | grep -P 'disk/[^/]+' )
+
+if (( ${#candidates[@]} == 0 )); then
+    >&2 echo "Error: found no VM image file in the container image"
+    exit 1
+elif (( ${#candidates[@]} > 1 )); then
+    >&2 echo "Error: found more than one VM image file in the container image"
+    exit 1
+fi
+
+tar -C "${temp_dir}" -xf "${temp_dir}/root.tar" "${candidates[0]}"
+chmod +w "${temp_dir}/${candidates[0]}"
+
+mv -f "${temp_dir}/${candidates[0]}" "${output_vm_image_file}"
