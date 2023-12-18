85311f9b249f14dbba83b6e68e0d5cc497154430
Alberto Faria <afaria@redhat.com>
2023-12-18 17:08:10 +0000

    Run virtiofsd as root
    This lets us use the chroot sandbox mode and simplifies things. We just
    need to pass it --modcaps=-mknod, since the container doesn't have that
    capability by default.
    
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- src/commands/entrypoint.sh
+++ src/commands/entrypoint.sh
@@ -23,35 +23,22 @@ echo 'group = "root"' >> /etc/libvirt/qemu.conf
 # disable libvirt cgroups management, since we're already in a container
 echo 'cgroup_controllers = []' >> /etc/libvirt/qemu.conf
 
-# we run virtiofsd as non-root so it doesn't try to drop capabilities, which
-# doesn't seem to work in unprivileged containers
-groupadd virtiofsd
-useradd --system --key MAIL_DIR=/dev/null --group virtiofsd virtiofsd
-
 virtlogd --daemon
 virtqemud --daemon
 
 # pass bind mounts through to the VM using virtiofs
 
 mkdir -p /crun-qemu/mounts/virtiofsd
-chown virtiofsd:virtiofsd /crun-qemu/mounts/virtiofsd
 
-# we run virtiofsd as non-root (see Containerfile), thus we can't use
-# --sandbox=chroot, but libvirt doesn't allow --sanbox=none, so we launch
-# virtiofsd ourselves instead
 mapfile -t mount_ids < <( find /crun-qemu/mounts -name '*[0-9]' -printf '%f\n' )
 for i in "${mount_ids[@]}"; do
-    runuser -u virtiofsd -g virtiofsd -- /usr/libexec/virtiofsd \
+    /usr/libexec/virtiofsd \
+        --modcaps=-mknod \
         --shared-dir "/crun-qemu/mounts/$i" \
         --socket-path "/crun-qemu/mounts/virtiofsd/$i" \
-        --sandbox none \
+        --sandbox chroot \
         &>"/crun-qemu/mounts/virtiofsd/$i.log" \
         &
-done
-
-for i in "${mount_ids[@]}"; do
-    while [[ ! -e "/crun-qemu/mounts/virtiofsd/$i" ]]; do sleep 0.1; done
-    chmod g+rw "/crun-qemu/mounts/virtiofsd/$i"
 done
 
 # launch VM
